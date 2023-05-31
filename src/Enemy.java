e24348368454d3f9aacc8574f13f7f956933a808
potatonuggetz <jaydenlhwong@gmail.com>
2023-05-31 17:19:35 -0400

    added variables to enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
 
+abstract public class Enemy {
+    protected String name;
+    protected double speed;
+    protected double health;
+    protected double armor;
+    protected double magicResist;
+    private static ArrayList<Enemy> enemyList = new ArrayList<Enemy>();
+    //TODO: crowd control
 
-public class Enemy {
-    
+    public Enemy(){
+
+        enemyList.add(this); 
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getHealth(){
+        return health;
+    }
+
+    public double getArmor(){
+        return armor;
+    }
+
+    public double getMagicResist(){
+        return magicResist;
+    }
+
+    public ArrayList<Enemy> getEnemyList(){
+        return enemyList;
+    }
 }

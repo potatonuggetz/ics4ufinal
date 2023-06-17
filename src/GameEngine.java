20bafecad3ed9d71e241839fe1697fc600707768
b0806 <tambrendan05@gmail.com>
2023-06-17 13:47:37 -0400

    engine & renderer setup

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class GameEngine {
@@ -5,11 +7,11 @@ public class GameEngine {
     public ArrayList<Tower> placedTowers;
     public ArrayList<Enemy> shownEnemies;
     public Level level;
-    public GameRenderer renderer;
 
     int gold;
     int health;
 
+    Tower selectedTower;
 
     //boolean menuToggle;
 
@@ -21,5 +23,31 @@ public class GameEngine {
         for (Enemy e : shownEnemies) {
             e.posX += e.speed;
         }
+    }
+
+    public void draw(JPanel panel, Graphics g) {
+        g.drawImage(this.level.background, 0, 0, null);
+
+        for (Enemy e : this.shownEnemies) {
+            g.drawImage(e.image, e.posX-10, e.posY-10, null);
+        }
+        for (Tower t : this.placedTowers) {
+            g.drawImage(t.image, t.posX, t.posY, null);
+        }
+    }
+
+    // menu at bottom, gold, health, background
+    public void drawGameLayout(Graphics g) {
+
+    }
+
+    // towers, enemies, "bullets"
+    public void drawGameLogic(Graphics g) {
+
+    }
+
+    // tower highlight, tower info, ability buttons
+    public void drawSelectedTower(Graphics g) {
+
     }
 }

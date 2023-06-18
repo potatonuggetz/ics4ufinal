7398474850f243578d907bb223b33ed7b5baf1d2
potatonuggetz <44147004+potatonuggetz@users.noreply.github.com>
2023-06-18 03:04:02 -0400

    ezreal

---
+++ src/game/Towers/Ezreal/Ezreal.java
@@ -0,0 +1,15 @@
+package game.Towers.Ezreal;
+import game.*;
+
+public class Ezreal extends Tower{
+    public Ezreal(int x,int y){
+        super(x,y);
+        name="Ezreal";
+        description="The Prodigal Explorer";
+        attackDamage=new double[]{0,10,20,30,40,40};
+        abilityPower=new double[]{0,0,0,0,0,0};
+        attackSpeed=new double[]{0,0.8,0.9,1.3,1.7,2};
+        projectileSpeed=75;
+        range=275;
+    }
+}
---
+++ src/game/Towers/Ezreal/EzrealQ.java
@@ -0,0 +1,22 @@
+package game.Towers.Ezreal;
+import game.*;
+public class EzrealQ extends Ability{
+    public EzrealQ(Tower t){
+        super(t);
+        name="Mystic Shot";
+        description="Ezreal fires a bolt of energy in the target direction that deals physical damage to the first enemy hit.";
+        cooldown=new double[]{1000,4,3.5,3,2,1};
+        projSizeX=35;
+        projSizeY=35;
+        range=400;
+        magicDamage=false;
+        scalingAD=3;
+        scalingAP=0;
+        pierce=1;
+    }
+    
+    public void use(int x,int y,GameEngine g){
+        timeLastUsed=GameEngine.getCurrentFrame();
+        g.addProjectile(new Projectile(tower, this, new Pair<Integer,Integer>(x,y)));
+    }
+}
